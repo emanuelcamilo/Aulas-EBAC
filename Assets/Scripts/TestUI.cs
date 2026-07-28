9514514d8ba01181324f7aea43dfc52e9f1a6ce7
Emanuel Camilo <emanuelcamilo664@gmail.com>
2026-07-27 21:06:49 -0300

    TestUI
    Desativando e Ativando imagem utilizando botoes na tela

---
+++ Assets/Scripts/TestUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TestUI : MonoBehaviour
+{
+
+    public GameObject image;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
